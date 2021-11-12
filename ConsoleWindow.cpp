52cd41184eb9d8a8eaa4832708fb1cf43abb4a50
ChNoir <94179306+ChNoir@users.noreply.github.com>
2021-11-12 14:42:13 +0100

    Add files via upload

---
+++ ConsoleWindow.cpp
@@ -0,0 +1,259 @@
+#include "ConsoleWindow.h"
+
+ConsoleWindow::ConsoleWindow(int X, int Y)
+{
+
+	std::string tamp = "mode " + std::to_string(X) + " , " + std::to_string(Y);
+	ConsoleWindow::cursor_Ymax = Y;
+	ConsoleWindow::cursor_Xmax = X;
+	system(tamp.c_str());
+
+}
+
+COORD ConsoleWindow::GetCursorPosition()
+{
+
+	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
+	return csbiInfo.dwCursorPosition ;
+}
+
+void ConsoleWindow::SetSizeConsoleWindow(int X, int Y)
+{
+	ConsoleWindow::cursorDefault();
+	system("cls");
+	std::string tamp = "mode " + std::to_string(X) + " , " + std::to_string(Y);
+	ConsoleWindow::cursor_Ymax = Y;
+	ConsoleWindow::cursor_Xmax = X;
+
+	system(tamp.c_str());
+}
+
+void ConsoleWindow::cursorCout(std::string text)
+{
+	std::cout << text;
+	int x = text.length();
+
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = cursor_Y;
+
+	cursor_X += x;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorCout(int i)
+{
+	std::cout << i;
+	int x = std::to_string(i).length();
+	
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = cursor_Y;
+	cursor_X += x;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+
+}
+
+void ConsoleWindow::cursorCout(long int i)
+{
+	std::cout << i;
+	int x = std::to_string(i).length();
+
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = cursor_Y;
+	cursor_X += x;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorCout(double i , int precision )
+{
+	std::ostringstream ss;
+	ss << std::fixed << std::setprecision(precision) << i;
+	std::string t = ss.str();
+	std::cout << t ;
+	int x = t.size();
+
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = cursor_Y;
+
+	cursor_X += x;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorDefault()
+{
+	COORD CRD;
+	CRD.X = 0;
+	CRD.Y = 0;
+
+	cursor_X = 0;
+	cursor_Y = 0;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorDefaultDown()
+{
+	
+	COORD CRD;
+	CRD.X = 0;
+	CRD.Y = cursor_Ymax;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorToMove(int x, int y)
+{
+	COORD CRD;
+	CRD.X = x;
+	CRD.Y = y;
+
+	cursor_X = x;
+	cursor_Y = y;
+
+	if (x >= cursor_Xmax) { 
+		CRD.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+	if (y >= cursor_Ymax) { 
+		CRD.Y = cursor_Ymax; 
+		cursor_Y = cursor_Ymax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorToMove(COORD coord)
+{
+	cursor_X = coord.X;
+	cursor_Y = coord.Y;
+
+	if (coord.X >= cursor_Xmax) {
+		coord.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+	if (coord.Y >= cursor_Ymax) {
+		coord.Y = cursor_Ymax;
+		cursor_Y = cursor_Ymax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}
+
+void ConsoleWindow::cursorMove(int x, int y)
+{
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = y + cursor_Y;
+
+	cursor_X += x;
+	cursor_Y += y;
+
+	if (CRD.X >= cursor_Xmax) {
+		CRD.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+	if (CRD.Y >= cursor_Ymax) {
+		CRD.Y = cursor_Ymax;
+		cursor_Y = cursor_Ymax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorMove(COORD coord)
+{
+
+	coord.X = coord.X + cursor_X;
+	coord.Y = coord.Y + cursor_Y;
+
+	cursor_X = coord.X;
+	cursor_Y = coord.Y;
+
+	if (coord.X >= cursor_Xmax) {
+		coord.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+	if (coord.Y >= cursor_Ymax) {
+		coord.Y = cursor_Ymax;
+		cursor_Y = cursor_Ymax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}
+
+void ConsoleWindow::cursorMoveLin(int y)
+{
+	COORD CRD;
+	CRD.X = cursor_X;
+	CRD.Y = y + cursor_Y;
+
+	cursor_Y += y;
+
+	if (CRD.Y >= cursor_Ymax) {
+		CRD.Y = cursor_Ymax;
+		cursor_Y = cursor_Ymax;
+	}
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorMoveLin(COORD coord)
+{
+	COORD CRD;
+	CRD.X = cursor_X;
+	CRD.Y = coord.Y + cursor_Y;
+
+	cursor_Y += coord.Y;
+
+	if (CRD.Y >= cursor_Ymax) {
+		CRD.Y = cursor_Ymax;
+		cursor_Y = cursor_Ymax;
+	}
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorMoveCol(int x)
+{
+	COORD CRD;
+	CRD.X = x + cursor_X;
+	CRD.Y = cursor_Y;
+
+	cursor_X += x;
+
+	if (CRD.X >= cursor_Xmax) {
+		CRD.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+}
+
+void ConsoleWindow::cursorMoveCol(COORD coord)
+{
+	COORD CRD;
+	CRD.X = coord.X + cursor_X;
+	CRD.Y = cursor_Y;
+
+	cursor_X += coord.X;
+
+	if (CRD.X >= cursor_Xmax) {
+		CRD.X = cursor_Xmax;
+		cursor_X = cursor_Xmax;
+	}
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CRD);
+
+}
+
+void ConsoleWindow::clearConsole()
+{
+	system("cls");
+	ConsoleWindow::cursorDefault();
+}
+
+
+
---
+++ ConsoleWindow.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h>
+#include <sstream>
+
+class ConsoleWindow
+{
+
+public:
+	
+	/// <summary>
+	/// /!\ Defini la taille maximal da la console de X colonne et Y ligne, le curseur ne poura pas depasse la limite.
+	/// Si l'envoi de caractère depase les limite de Y, la ligne 0 sera supprime et tout sera deplace de -1 Y.  
+	/// </summary>
+	ConsoleWindow(int X, int Y);
+
+	/// Retourne une struct avec les coordonne du curseur.
+	COORD GetCursorPosition();
+
+	/// Defini la taille da la console de X colonne et Y ligne.
+	void SetSizeConsoleWindow(int X, int Y);
+
+	/// Fait deplace le cureur avec l'ecriture du "cout".
+	void cursorCout(std::string text);
+	void cursorCout(int i);
+	void cursorCout(long int i);
+	void cursorCout(double i, int precision = 12 );
+
+	/// Deplace le curseur en [ X = 0 ; Y = 0] 
+	void cursorDefault();
+
+	/// Deplace le curseur en [ X = 0 ; Y = max ]
+	void cursorDefaultDown();
+
+	/// Deplace le curseur a la position demande, ne peux pas depasse les limite de la console 
+	void cursorToMove(int x, int y);
+	void cursorToMove(COORD coord);
+
+	/// Deplace le curseur de X et de Y depuis sa position actuelle, ne peux pas depasse les limite de la console   
+	void cursorMove(int x, int y);
+	void cursorMove(COORD coord);
+
+	/// Deplace le curseur de Y depuis sa position actuelle, ne peux pas depasse les limite de la console  
+	void cursorMoveLin(int y);
+	void cursorMoveLin(COORD coord);
+
+	/// Deplace le curseur de X depuis sa position actuelle, ne peux pas depasse les limite de la console  
+	void cursorMoveCol(int x);
+	void cursorMoveCol(COORD coord);
+
+	/// Clear la console 
+	void clearConsole();
+
+	/// Clear la console et remet le curseur a 0
+	
+
+
+private:
+
+	int cursor_Y = 0;
+	int cursor_X = 0;
+	int cursor_Ymax;
+	int cursor_Xmax;
+
+};
+
